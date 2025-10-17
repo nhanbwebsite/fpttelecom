6ce60ea3c0433ed9c682e1d405cbf00175f03ec4
nhanbwebsite <nhanbwebsite@gmail.com>
2025-10-17 09:51:58 +0700

    okay

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -43,14 +43,15 @@
 
     {{-- ads --}}
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-DT9YH6L0KB"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16728404203"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-DT9YH6L0KB');
+  gtag('config', 'AW-16728404203');
     </script>
+
 
     @stack('styles')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
